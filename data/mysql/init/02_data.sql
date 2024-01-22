2e43e7f3f05085a5c87c4c274b49f5f66b623cde
Dave Paroulek <david.paroulek@camunda.com>
2024-01-22 17:18:51 -0500

    Adding support for mssql

--- data/mysql/init/02_data.sql
+++ data/mysql/init/02_data.sql
@@ -1,24 +1,24 @@
 -- USERS
 
-INSERT INTO users (username, password, email, first_name, last_name, created_at)
+INSERT INTO users (username, password, email, first_name, last_name)
 VALUES ('user1', 'password1', 'user1@email.com', 'John', 'Doe');
-INSERT INTO users (username, password, email, first_name, last_name, created_at)
-VALUES ('user2', 'password2', 'user2@email.com', 'Jane', 'Smith', CURRENT_TIMESTAMP());
-INSERT INTO users (username, password, email, first_name, last_name, created_at)
-VALUES ('user3', 'password3', 'user3@email.com', 'Bob', 'Johnson', CURRENT_TIMESTAMP());
-INSERT INTO users (username, password, email, first_name, last_name, created_at)
-VALUES ('user4', 'password4', 'user4@email.com', 'Sally', 'Williams', CURRENT_TIMESTAMP());
-INSERT INTO users (username, password, email, first_name, last_name, created_at)
-VALUES ('user5', 'password5', 'user5@email.com', 'Tom', 'Brown', CURRENT_TIMESTAMP());
-INSERT INTO users (username, password, email, first_name, last_name, created_at)
-VALUES ('user6', 'password6', 'user6@email.com', 'Linda', 'Jones', CURRENT_TIMESTAMP());
-INSERT INTO users (username, password, email, first_name, last_name, created_at)
-VALUES ('user7', 'password7', 'user7@email.com', 'Alex', 'Miller', CURRENT_TIMESTAMP());
-INSERT INTO users (username, password, email, first_name, last_name, created_at)
-VALUES ('user8', 'password8', 'user8@email.com', 'Lisa', 'Davis', CURRENT_TIMESTAMP());
-INSERT INTO users (username, password, email, first_name, last_name, created_at)
-VALUES ('user9', 'password9', 'user9@email.com', 'David', 'Garcia', CURRENT_TIMESTAMP());
-INSERT INTO users (username, password, email, first_name, last_name, created_at)
-VALUES ('user10', 'password10', 'user10@email.com', 'Katie', 'Martinez', CURRENT_TIMESTAMP());
-INSERT INTO users (username, password, email, first_name, last_name, created_at)
-VALUES ('user11', 'password11', 'user11@email.com', 'Mike', 'Robinson', CURRENT_TIMESTAMP());
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES ('user2', 'password2', 'user2@email.com', 'Jane', 'Smith');
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES ('user3', 'password3', 'user3@email.com', 'Bob', 'Johnson');
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES ('user4', 'password4', 'user4@email.com', 'Sally', 'Williams');
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES ('user5', 'password5', 'user5@email.com', 'Tom', 'Brown');
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES ('user6', 'password6', 'user6@email.com', 'Linda', 'Jones');
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES ('user7', 'password7', 'user7@email.com', 'Alex', 'Miller');
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES ('user8', 'password8', 'user8@email.com', 'Lisa', 'Davis');
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES ('user9', 'password9', 'user9@email.com', 'David', 'Garcia');
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES ('user10', 'password10', 'user10@email.com', 'Katie', 'Martinez');
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES ('user11', 'password11', 'user11@email.com', 'Mike', 'Robinson');
---
+++ src/test/resources/data-mssql.sql
@@ -0,0 +1,18 @@
+-- USERS
+
+INSERT INTO [users] (username, password, email, first_name, last_name)
+VALUES
+    ('user1', 'password1', 'user1@email.com', 'John', 'Doe'),
+    ('user2', 'password2', 'user2@email.com', 'Jane', 'Smith'),
+    ('user3', 'password3', 'user3@email.com', 'Bob', 'Johnson'),
+    ('user4', 'password4', 'user4@email.com', 'Sally', 'Williams'),
+    ('user5', 'password5', 'user5@email.com', 'Tom', 'Brown'),
+    ('user6', 'password6', 'user6@email.com', 'Linda', 'Jones'),
+    ('user7', 'password7', 'user7@email.com', 'Alex', 'Miller'),
+    ('user8', 'password8', 'user8@email.com', 'Lisa', 'Davis'),
+    ('user9', 'password9', 'user9@email.com', 'David', 'Garcia'),
+    ('user10', 'password10', 'user10@email.com', 'Katie', 'Martinez'),
+    ('user11', 'password11', 'user11@email.com', 'Mike', 'Robinson')
+GO
+
+
